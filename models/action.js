392ee4394d0fc38e1f8267d6045f2f36fdf6d854
elf Pavlik <perpetual-tripper@wwelves.org>
2013-10-15 12:35:05 +0200

    removed cruft from Action

--- models/action.js
+++ models/action.js
@@ -5,44 +5,8 @@ var Action = Backbone.Model.extend({
   idAttribute: 'uuid',
 
   initialize: function() {
-    // for new capture
-    if(!this.get('uuid')){
-      this.set({
-        '@type': 'capture',
-        uuid: uuid(),
-        timeStart: new Date().getTime()
-      });
-    }
   },
 
-  /*
-   * pictures: 'image/*'
-   * videos: 'video/*'
-   * audio rec: 'video/3gpp'
-   *
-   * used to attach media files to capture
-   * #attribution http://blog.w3villa.com/websites/uploading-filesimage-with-ajax-jquery-without-submitting-a-form/
-   */
-  attachFile: function(mediaUrl, file) {
-    if(! file) throw new TypeError("attachFile(mediaUrl, file) didn't get enough parameters");
-    this.set('mediaType', file.type);
-    var formData = new FormData();
-    var metadata = { uuid: this.get('uuid') };
-    formData.append('file', file);
-    formData.append('meta', JSON.stringify(metadata));
-    $.ajax({
-      url: mediaUrl,
-      data: formData,
-      type: 'post',
-      contentType: false,
-      processData: false
-    });
-  },
-
-  // for now we use for marker location where capture got submited
-  markerLocation: function() {
-    return this.get('locationSubmit');
-  }
 });
 
 module.exports = Action;
