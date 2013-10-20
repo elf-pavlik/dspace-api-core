818c22ba885100e11e710dbec01748bdf52d52f7
elf Pavlik <perpetual-tripper@wwelves.org>
2013-10-20 11:48:03 +0200

    changed remote RemotePlayer geoloation to depend on global dspace

--- models/remotePlayer.js
+++ models/remotePlayer.js
@@ -6,7 +6,8 @@ var RemotePlayer = Player.extend({
   // FIXME what if no uuid?
   initialize: function(attrs, options) {
 
-    this.geolocation = options.dspace.getGeolocationChannel(this.get('channels').track);
+    //FIXME don't expect global dspace
+    this.geolocation = dspace.getGeolocationChannel(this.get('channels').track);
 
     // super
     Player.prototype.initialize.call(this, attrs, options);
