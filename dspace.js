627de1cdc794b730526aa9d21c17d9a441f55423
elf Pavlik <perpetual-tripper@wwelves.org>
2014-01-27 15:57:50 +0100

    imported creation of localPlayer and map from dspace-action-slim

--- dspace.js
+++ dspace.js
@@ -1,10 +1,11 @@
 var Nexus = require('./nexus');
 var Party = require('./collections/party');
+var LocalPlayer = require('dspace-api-core/models/localPlayer');
 
-var DSpace = function(map, Roster, BayeuxHub, config){
+var DSpace = function(Map, Roster, BayeuxHub, config){
 
   this.config = config;
-  this.map = map;
+  this.map = new Map({config: config.map});
 
   this.nexus = new Nexus(config, BayeuxHub);
 
@@ -19,6 +20,28 @@ var DSpace = function(map, Roster, BayeuxHub, config){
     // #attribution: http://www.paulirish.com/2009/random-hex-color-code-snippets/
     randomColor: function(){ return '#' + Math.floor(Math.random()*16777215).toString(16); }
   };
+
+  this.player = new LocalPlayer(this.config.player.profile, {
+    settings: this.config.settings,
+    nexus: this.nexus
+  });
+
+  this.player.on('change', function(player){
+    localStorage.profile = JSON.stringify(player);
+  });
+
+  // FIXME move to Roster?
+  this.player.layerGroup = new L.LayerGroup();
+  this.player.layerGroup.addTo(this.map.frame);
+  this.roster.createPlayerOverlays(this.player, { avatar: this.player.layerGroup, track: this.player.layerGroup });
+
+  // PLAY!
+  this.publishPlayer = function(player){
+    this.party.portal.channel.pub(player);
+  };
+  this.publishPlayer(this.player.toJSON());
+  this.player.on('change', this.publishPlayer);
+
 };
 
 module.exports = DSpace;
